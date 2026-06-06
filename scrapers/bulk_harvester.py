687d9b4568c5de197ab18a238c9c7edd8b84068c
samyakshingi <samyakshingi9@gmail.com>
2026-06-06 16:35:12 +0530

    chore(scraper): restore full 5 URLs list in bulk_harvester.py

--- scrapers/bulk_harvester.py
+++ scrapers/bulk_harvester.py
@@ -69,7 +69,10 @@ def parse_with_gemini(markdown_content: str, url: str) -> Optional[TourPackage]:
 async def main():
     urls = [
         "https://www.veenaworld.com/package/magical-kashmir-tour-package-haqm",
-        "https://www.thrillophilia.com/tours/leh-ladakh-tour-packages"
+        "https://www.thrillophilia.com/tours/leh-ladakh-tour-packages",
+        "https://www.thrillophilia.com/tours/manali-tour-packages",
+        "https://www.thrillophilia.com/tours/kashmir-tour-packages",
+        "https://www.veenaworld.com/package/shimla-manali-tour-package-shms"
     ]
     
     supabase_url = os.environ.get("SUPABASE_URL")
